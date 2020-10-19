5bf892519cebcb552462c8cc2b00e6c1b5b87afd
liuhongdi <liuhongdi@gmail.com>
2020-10-19 13:36:35 +0800

    init

---
+++ src/main/java/com/masterslave/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.masterslave.demo;
+
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.transaction.jta.JtaAutoConfiguration;
+
+
+@SpringBootApplication(exclude = {DruidDataSourceAutoConfigure.class, JtaAutoConfiguration.class})
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
